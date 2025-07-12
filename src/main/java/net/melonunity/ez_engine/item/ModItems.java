21cabbabdad1eec987268bd7f21cb05533064946
MelonVRCoder <szwaiczykcastiel80@gmail.com>
2025-07-12 17:15:57 +0200

    Added 2 Items with One Creative Mod Tab

---
+++ src/main/java/net/melonunity/ez_engine/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.melonunity.ez_engine.item;
+
+import net.melonunity.ez_engine.EzEngine;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, EzEngine.MOD_ID);
+
+    public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
